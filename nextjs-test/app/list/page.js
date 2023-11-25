9c97a80433fb8c3cfb0ce9036b63dbd0bc95e02a
HanGyeongjun <030212hgjun@gmail.com>
2023-11-25 17:03:55 +0900

    [Feat] CodingApple NextJS lesson 1 to 3

---
+++ nextjs-test/app/list/page.js
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+
+export default function List() {
+
+  return (
+    <div>
+      <h4 className="title">Apple</h4>
+      <div className='appleProduct'>
+        <h4>iPhone 15</h4>
+      </div>
+      <div className='appleProduct'>
+        <h4>MacBook Pro</h4>
+      </div>
+    </div>
+  )
+}
---
+++ nextjs-test/app/list/product/page.js
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+
+export default function Product() {
+
+  return (
+    <div>
+      <h4 className="title">Product-iPhone</h4>
+      <div className='appleProduct'>
+        <h4>iPhone 15</h4>
+      </div>
+      <div className='appleProduct'>
+        <h4>MacBook Pro</h4>
+      </div>
+    </div>
+  )
+}
